68cb118121cc915b1a89605e518d0126448169e1
kevin.talmadge@eluv.io <kevin@eluv.io>
2022-09-09 12:12:54 -0700

    Update logo

--- webpack.config.js
+++ webpack.config.js
@@ -100,12 +100,7 @@ module.exports = {
       },
       {
         test: /\.(gif|png|jpe?g)$/i,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader"
-          },
-        ],
+        use: ["file-loader"],
       },
       {
         test: /\.(txt|bin|abi)$/i,
